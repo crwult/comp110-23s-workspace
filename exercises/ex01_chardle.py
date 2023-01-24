d2981a71de70332cf118d8d582f6fd56f214b6d4
Charles Wilt <croc@ad.unc.edu>
2023-01-23 22:50:42 -0500

    fixing lint pt.2

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -9,28 +9,28 @@ letter = input("Enter a single character: ")
 if len(letter) != 1:
     print("Error: Character must be a single character")
     exit()
-print("Searching for" , letter , "in" , word)
+print("Searching for", letter, "in", word)
 
 count = 0
 if word[0] == letter:
-    print(letter , "found at index 0")
+    print(letter, "found at index 0")
     count += 1
 if word[1] == letter:
-    print(letter , "found at index 1")
+    print(letter, "found at index 1")
     count += 1
 if word[2] == letter:
-    print(letter , "found at index 2")
+    print(letter, "found at index 2")
     count += 1
 if word[3] == letter:
-    print(letter , "found at index 3")
+    print(letter, "found at index 3")
     count += 1
 if word[4] == letter:
-    print(letter , "found at index 4")
+    print(letter, "found at index 4")
     count += 1
 
 if count == 0:
-    print("No instances of" , letter , "found in" , word)
+    print("No instances of", letter, "found in", word)
 if count == 1:
-    print("1 instance of" , letter , "found in" , word)
-if count>=2:
-    print(count , "instances of" , letter , "found in" , word)
+    print("1 instance of", letter, "found in", word)
+if count >= 2:
+    print(count, "instances of", letter, "found in", word)
