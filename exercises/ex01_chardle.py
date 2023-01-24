6f4526cbc44f788ee5d9481c55b7a38a1f60bebc
Charles Wilt <croc@ad.unc.edu>
2023-01-23 22:46:23 -0500

    Progress on Exercise 1. Fixed lint

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,36 +1,36 @@
-"""EX01 - Chardle - A cute step towards wordle"""
-__author__= "730552290"
+"""EX01 - Chardle - A cute step towards wordle."""
+__author__ = "730552290"
 
-word=input("Enter a 5-character word: ")
-if len(word)!=5:
+word = input("Enter a 5-character word: ")
+if len(word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
-letter= input("Enter a single character: ")
-if len(letter)!=1:
+letter = input("Enter a single character: ")
+if len(letter) != 1:
     print("Error: Character must be a single character")
     exit()
-print("Searching for",letter,"in",word)
+print("Searching for" , letter , "in" , word)
 
-count=0
-if word[0]==letter:
-    print(letter,"found at index 0")
-    count+=1
-if word[1]==letter:
-    print(letter,"found at index 1")
-    count+=1
-if word[2]==letter:
-    print(letter,"found at index 2")
-    count+=1
-if word[3]==letter:
-    print(letter,"found at index 3")
-    count+=1
-if word[4]==letter:
-    print(letter,"found at index 4")
-    count+=1
+count = 0
+if word[0] == letter:
+    print(letter , "found at index 0")
+    count += 1
+if word[1] == letter:
+    print(letter , "found at index 1")
+    count += 1
+if word[2] == letter:
+    print(letter , "found at index 2")
+    count += 1
+if word[3] == letter:
+    print(letter , "found at index 3")
+    count += 1
+if word[4] == letter:
+    print(letter , "found at index 4")
+    count += 1
 
-if count==0:
-    print("No instances of",letter,"found in",word)
-if count==1:
-    print("1 instance of",letter,"found in",word)
+if count == 0:
+    print("No instances of" , letter , "found in" , word)
+if count == 1:
+    print("1 instance of" , letter , "found in" , word)
 if count>=2:
-    print(count,"instances of",letter,"found in",word)
+    print(count , "instances of" , letter , "found in" , word)
